677683b492007cbbc2388fc5f7b830465a136f65
d11116219 <126222904+d11116219@users.noreply.github.com>
2026-03-31 16:15:57 +0800

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿namespace Class_0331_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Student s = new Student();
+            Console.WriteLine(s.Name);
+        }
+    }
+}
